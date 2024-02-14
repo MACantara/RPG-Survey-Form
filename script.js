ec3b9e67ff547168b4424d8e6f91b1ffaae66224
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-02-15 02:38:08 +0800

    Add Name Validation

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+document.getElementById('name').addEventListener('input', function (e) {
+    const pattern = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
+    if (e.target.value.match(pattern)) {
+        e.target.setCustomValidity('');
+    } else {
+        e.target.setCustomValidity(' Please enter a valid name');
+    }
+});
+
 document.getElementById('email').addEventListener('input', function (e) {
-    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (e.target.value.match(pattern)) {
         e.target.setCustomValidity('');
     } else {
